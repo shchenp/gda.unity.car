8ea2c8fc91c08e6d270730d2289dd82d4145cec2
Nikita <np.shche@gmail.com>
2023-11-09 23:25:10 +0300

    feat: add movemeent by car

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        var position = transform.position;
+        
+        var step = _speed * Time.deltaTime;
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.x += step;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            position.x -= step;
+        }
+
+        if (Input.GetKey(KeyCode.A) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+        {
+            position.z += step / 2;
+        }
+        else if (Input.GetKey(KeyCode.D) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
+        {
+            position.z -= step / 2;
+        }
+
+        transform.position = position;
+    }
+}
